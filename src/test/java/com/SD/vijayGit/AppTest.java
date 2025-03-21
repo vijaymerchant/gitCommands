937b8db8b54a0ea3db8e6d323114733db8d086a3
v-vijay.mishra2 <v-vijay.mishra2@yatra.com>
2025-03-21 15:29:40 +0530

    first comit

---
+++ src/test/java/com/SD/vijayGit/AppTest.java
@@ -0,0 +1,13 @@
+package com.SD.vijayGit;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+	public static void main(String args[]) {
+		System.out.print("Hello world");
+	} 
+
+}
